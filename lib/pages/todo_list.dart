6803a484d2fc43e1f094bce88cae72b0f48c5042
Arley Souto <macx15@hotmail.com>
2023-12-23 01:05:58 -0300

    feat: separando arquivos

---
+++ lib/pages/todo_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget { //se precisar ter um estado na página é só transformar a extensão em Stateless
+  const TodoListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.yellow,);
+  }
+}
