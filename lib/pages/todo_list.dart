638c514f9ebbfea8832d52f5e5c54c47acbc8f03
Arley Souto <macx15@hotmail.com>
2024-01-04 03:02:17 -0300

    feat: criado o campo com o botão

--- lib/pages/todo_list.dart
+++ lib/pages/todo_list.dart
@@ -2,46 +2,42 @@ import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
   //se precisar ter um estado na página é só transformar a extensão em Stateless
-  TodoListPage({super.key});
-
-  //cada compo de texto vai ter um controller, quando o controller é adicionado a const do super tem que ser removida
-  final TextEditingController emailController = TextEditingController();
+  const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //temos sempre que usar o Scaffold
       body: Center(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        child: Row(
           children: [
-          TextField(
-            controller: emailController,
-            decoration: InputDecoration(
-                labelText: 'Email', //esse aqui é o label
-                hintText: '@exemplo.com', //aqui é o placeholder
-                border: OutlineInputBorder()),
-                onChanged: onChanged,
-                onSubmitted: onSubmitted ,
-          ),
-          ElevatedButton(onPressed: login, child: Text('Entrar'))
-        ]),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma tarefa',
+                    hintText: 'Ex. Estudar Flutter'),
+              ),
+            ),
+            SizedBox(width: 8),
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(
+                Icons.add,
+                size: 30,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff00d7f3),
+                  padding: EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  )),
+            )
+          ],
+        ),
       )),
     );
-  }
-
-  void login(){
-    String text = emailController.text;
-    print('entrar $text');
-  }
-
-  void onChanged(String text){
-    print(text);
-  }
-
-  void onSubmitted(String text){
-    print('submited $text');
   }
 }
