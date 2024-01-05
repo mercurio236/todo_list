47f1cc2ee8cdff4b92406aa0c9ed64508c70b255
Arley Souto <macx15@hotmail.com>
2024-01-04 21:10:46 -0300

    feat: criado botão para limpar lista

--- lib/pages/todo_list.dart
+++ lib/pages/todo_list.dart
@@ -10,30 +10,58 @@ class TodoListPage extends StatelessWidget {
       body: Center(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Row(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma tarefa',
-                    hintText: 'Ex. Estudar Flutter'),
-              ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex. Estudar Flutter'),
+                  ),
+                ),
+                SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.add,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(22),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      )),
+                )
+              ],
             ),
-            SizedBox(width: 8),
-            ElevatedButton(
-              onPressed: () {},
-              child: Icon(
-                Icons.add,
-                size: 30,
-                color: Colors.white,
-              ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff00d7f3),
-                  padding: EdgeInsets.all(20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6),
-                  )),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                Expanded(child: Text('Você possui 0 tarefas pendentes')),
+                SizedBox(
+                  width: 8,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Limpar tudo',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(22),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                )
+              ],
             )
           ],
         ),
